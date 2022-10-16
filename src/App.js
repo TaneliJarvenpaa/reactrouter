29669add4adb46c075f9a18f01be23fc4673a5e1
TaneliJarvenpaa <114722920+TaneliJarvenpaa@users.noreply.github.com>
2022-10-16 14:58:13 +0300

    tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import {  BrowserRouter,  Routes,  Route,  Link} from"react-router-dom";
 
 function App() {
+  
+
+
+function Home() {
+  return (
+    <div>
+      <h2>This is Home page</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>This is About page</h2>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>This is Contact page</h2>
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div>
+       <Link to="/">Empty, go to home page</Link>
+    </div>
+  );
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Welcome to React Router
+       </header>
+       <BrowserRouter>
+       <Link to="/"> Home </Link>{' '}
+       <Link to="/about"> About </Link>{' '}
+       <Link to="/contact">Contact</Link>{' '}
+       <Routes>
+        <Route exact path="/"element={<Home />} />
+        <Route path="/about"element={<About />} />
+        <Route path="/contact"element={<Contact />} />
+        <Route path="*"element={<NotFound />} />
+        </Routes>
+       </BrowserRouter>
+        
+     
+      
     </div>
   );
 }
